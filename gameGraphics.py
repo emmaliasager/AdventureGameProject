2bd01873e69c27fa9e178d62d27b3e7cb232cb89
emmaliasager <emmalia.sager@umconnect.umt.edu>
2024-11-24 19:34:51 -0700

    Wandering Monster

--- gameGraphics.py
+++ gameGraphics.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -9,16 +10,50 @@ WIDTH, HEIGHT = GRID_SIZE * SQUARE_SIZE, GRID_SIZE * SQUARE_SIZE
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Move the Square!")
+pygame.display.set_caption("Monster Encounter Game!")
+
+clock = pygame.time.Clock()
 
 x, y = 0, 0
 
+class WanderingMonster:
+    def __init__(self, grid_size):
+        self.grid_size = grid_size
+        self.x = random.randint(0, grid_size - 1) * SQUARE_SIZE
+        self.y = random.randint(0, grid_size - 1) * SQUARE_SIZE
+
+    def move(self):
+        directions = ['up', 'down', 'left', 'right']
+        direction= random.choice(directions)
+
+        if direction == 'up' and self.y > 0:
+            self.y -= SQUARE_SIZE
+        elif direction == 'down' and self.y < (self.grid_size -1) * SQUARE_SIZE:
+            self.y += SQUARE_SIZE
+        elif direction == 'left' and self.x > 0:
+            self.x -= SQUARE_SIZE
+        elif direction == 'right' and self.x < (self.grid_size -1) * SQUARE_SIZE:
+            self.x += SQUARE_SIZE
+
+    def get_location(self):
+        return (self.x, self.y)
+
+def check_encounter(player_pos, monsters):
+    for monster in monsters:
+        if player_pos == monster.get_location():
+            print("Encounter! You ran into a monster!")
+            return True
+    return False
+
+monsters = [WanderingMonster(GRID_SIZE)]
 
 running = True
-while running:
+player_pos = (x, y)
 
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -34,6 +69,16 @@ while running:
             elif event.key == pygame.K_q:
                 running = False
 
+    player_pos = (x, y)
+
+    if check_encounter(player_pos, monsters):
+        print("Game Over!")
+        running = False
+
+    for monster in monsters:
+        monster.move()
+        
+    
     screen.fill(WHITE)
 
     pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, SQUARE_SIZE, SQUARE_SIZE))
